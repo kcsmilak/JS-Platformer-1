5fdf31cd4f664d2d7df29346f84bb93d79895083
Kevin Smilak <kevin@smilak.com>
2022-04-05 15:08:51 +0000

    basic scrolling

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ class Game {
         this.gameMap = new GameMap()
         this.tiles = []
 
-        this.scrollBox = new Rectangle(50,150,400,200)
+        this.camera = new Rectangle(50,150,400,200)
         
         this.showTileMap = false
         this.showGameMap = false
@@ -58,14 +58,15 @@ class Game {
         this.player.update(this.tiles)
 
 
-        let xmargin = 0
-        if (this.player.x < this.scrollBox.x + xmargin) {
-            this.scrollBox.x = this.player.x - xmargin
-        } else if (this.player.x + this.player.width > this.scrollBox.x + this.scrollBox.width - xmargin) {
-            this.scrollBox.x = this.player.x - this.scrollBox.width + this.player.width + xmargin
+        let xmargin = 100
+        if (this.player.x < this.camera.x + xmargin) {
+            this.camera.x = this.player.x - xmargin
+        } else if (this.player.x + this.player.width > this.camera.x + this.camera.width - xmargin) {
+            this.camera.x = this.player.x - this.camera.width + this.player.width + xmargin
         }
-        if (this.scrollBox.x < 50) this.scrollBox.x = 50
-        if (this.scrollBox.x > 300) this.scrollBox.x = 300        
+        if (this.camera.x < 0) this.camera.x = 0
+        let max = 900
+        if (this.camera.right > max) this.camera.right = max        
         
     }
 
@@ -75,7 +76,9 @@ class Game {
 
 
         push()
-        translate(-this.scrollBox.x+50,0)
+        scale(1.5)
+        translate(-this.camera.x, -this.camera.y)
+        //
         this.player.draw()
 
         this.tiles.forEach(tile => {
@@ -85,7 +88,7 @@ class Game {
         push()
         strokeWeight(3)
         fill(0,0,0,0)
-        rect(this.scrollBox.x, this.scrollBox.y, this.scrollBox.width, this.scrollBox.height)
+        //rect(this.camera.x, this.camera.y, this.camera.width, this.camera.height)
         pop()
         pop()
 
